bc6fc8c74bed6dbb17d583e8be129d9e4dd764d9
Samuel Felton <78095426+SamFeltip@users.noreply.github.com>
2024-06-07 17:23:32 +0100

    removed example.co.uk

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://sharrowvale.co.uk',
   integrations: [mdx(), sitemap(), react(), tailwind({
     applyBaseStyles: false
   }), pagefind()]
