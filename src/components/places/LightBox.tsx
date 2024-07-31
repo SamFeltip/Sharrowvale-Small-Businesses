0514eba32e0d444a11061d7ce159e0959d451aa2
Samuel Felton <78095426+SamFeltip@users.noreply.github.com>
2024-07-31 17:12:39 +0100

    remove imports

--- src/components/places/LightBox.tsx
+++ src/components/places/LightBox.tsx
@@ -1,13 +1,7 @@
-// import { useCallback, useEffect, useRef, useState } from "react";
-// import LightGallery from 'lightgallery/react';
-// import lgZoom from 'lightgallery/plugins/zoom';
-
 import * as React from "react";
 import Lightbox, { type SlideImage, type ThumbnailsRef } from "yet-another-react-lightbox";
 import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
 import "yet-another-react-lightbox/plugins/thumbnails.css";
-
-import { Button } from "@/components/ui/button";
 
 
 import "yet-another-react-lightbox/styles.css";
@@ -77,59 +71,3 @@ export default function LightBox({ slides }: { slides: SlideImage[] }) {
         </>
     );
 }
-
-
-
-// export default function LightBox() {
-//     const lightGallery = useRef<any>(null);
-//     const [items, setItems] = useState([
-//         {
-//             id: '1',
-//             size: '1400-800',
-//             src: 'https://images.unsplash.com/photo-1455541504462-57ebb2a9cec1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=320&q=80',
-//             thumb: 'https://images.unsplash.com/photo-1455541504462-57ebb2a9cec1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=320&q=80',
-//         },
-//         {
-//             id: '2',
-//             size: '1400-800',
-//             src: 'https://images.unsplash.com/photo-1584592740039-cddf0671f3d4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=240&q=80',
-//             thumb: 'https://images.unsplash.com/photo-1584592740039-cddf0671f3d4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=240&q=80',
-//         },
-//     ]);
-
-//     const onInit = useCallback((detail: { instance: any; }) => {
-//         if (detail) {
-//             lightGallery.current = detail.instance;
-//         }
-//     }, []);
-
-//     const getItems = useCallback(() => {
-//         return items.map((item) => {
-//             return (
-//                 <div
-//                     key={item.id}
-//                     data-lg-size={item.size}
-//                     className="gallery-item"
-//                     data-src={item.src}
-//                 >
-//                     <img alt="" className="img-responsive" src={item.thumb} />
-//                 </div>
-//             );
-//         });
-//     }, [items]);
-
-//     useEffect(() => {
-//         lightGallery.current.refresh();
-//     }, [items]);
-
-//     return (
-//         <div className="App">
-//             <LightGallery
-//                 plugins={[lgZoom]}
-//                 onInit={onInit}
-//             >
-//                 {getItems()}
-//             </LightGallery>
-//         </div>
-//     );
-// }
