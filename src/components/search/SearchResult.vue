7b7a70a72aa202bbe0c097a5167a324bfbe0cf9a
Samuel Felton <78095426+SamFeltip@users.noreply.github.com>
2024-11-25 14:16:46 +0000

    added hover article to search result

--- src/components/search/SearchResult.vue
+++ src/components/search/SearchResult.vue
@@ -1,32 +1,36 @@
 <template>
-    <a :href="props.href">
-        <div id="search-result" class="rounded-lg overflow-hidden">
-            <img
-                v-if="image"
-                :src="image"
-                :alt="title"
-                class="w-full max-w-[300px] h-48 object-cover rounded-lg"
-            />
-            <div class="w-full p-4">
-                <div class="flex gap-2">
-                    <a
-                        v-for="(category, index) in categories"
-                        :key="index"
-                        class="text-coral px-2 py-1 text-sm"
-                    >
-                        {{ category.name }}
-                    </a>
+    <HoverArticle>
+        <a :href="props.href">
+            <div id="search-result" class="rounded-lg overflow-hidden">
+                <img
+                    v-if="image"
+                    :src="image"
+                    :alt="title"
+                    class="w-full max-w-[300px] h-48 object-cover rounded-lg"
+                />
+                <div class="w-full p-4">
+                    <div class="flex gap-2">
+                        <a
+                            v-for="(category, index) in categories"
+                            :key="index"
+                            class="text-coral px-2 py-1 text-sm"
+                        >
+                            {{ category.name }}
+                        </a>
+                    </div>
+                    <h3 class="text-lg font-semibold mb-2">
+                        {{ title }}
+                    </h3>
+                    <p class="mb-4">{{ content }}</p>
                 </div>
-                <h3 class="text-lg font-semibold mb-2">
-                    {{ title }}
-                </h3>
-                <p class="mb-4">{{ content }}</p>
             </div>
-        </div>
-    </a>
+        </a>
+    </HoverArticle>
 </template>
 
 <script setup lang="ts">
+import HoverArticle from "../elements/articles/HoverArticle.vue";
+
 const props = defineProps<{
     image: string;
     title: string;
