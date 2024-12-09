33c156925baa9d0dcb1dd632dc2cbc494774127f
Sam Felton <sf.samfelton@icloud.com>
2024-12-09 09:11:02 +0000

    added business carousel at the bottom of the page page

--- src/scripts/placeCard/genericPlaceCard/getPlaceCardRefs.ts
+++ src/scripts/placeCard/genericPlaceCard/getPlaceCardRefs.ts
@@ -8,15 +8,6 @@ export function getPlaceCardRefs(
     genericCollectionEntry: GenericCollectionEntry[]
 ): PlaceCardRef[] {
     return genericCollectionEntry.map((item) => {
-        if (item.data.heroImage == null) {
-            throw new Error(
-                "tags must include a hero images to be matched to a PlaceCardRef"
-            );
-        }
-
-        console.log("***");
-        console.log(item.data.heroImage);
-
         return {
             type: type,
             collectionEntry: item,
