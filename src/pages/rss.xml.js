ac39511c36bbef3cc4a39c6b39e2d6ce08eb3df3
Sam Felton <sf.samfelton@icloud.com>
2025-03-24 23:22:55 +0000

    fixed article sort in rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,11 +4,17 @@ import { getCollection } from "astro:content";
 
 export async function GET(context) {
     const articles = await getCollection("articles");
+
+    const sortedArticles = articles.sort(
+        (a, b) =>
+            new Date(b.data.publishDate).getTime() -
+            new Date(a.data.publishDate).getTime()
+    );
     return rss({
         title: SITE_TITLE,
         description: SITE_DESCRIPTION,
         site: context.site,
-        items: articles.map((article) => ({
+        items: sortedArticles.map((article) => ({
             title: article.data.name,
             pubDate: article.data.publishDate,
             description: article.data.description,
